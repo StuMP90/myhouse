9f1e0fec3db66375223477a9217ad9c06a3646f7
StuMP90 <gitstm90@dsv1.co.uk>
2022-04-07 15:38:53 +0100

    Add update index

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +12,12 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
         return new Response(<<<EOF
 <html>
     <body>
