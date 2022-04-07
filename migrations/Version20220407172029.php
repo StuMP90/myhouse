d5feb69780dc07a027233dd618d09cb402bfb3ec
StuMP90 <gitstm90@dsv1.co.uk>
2022-04-07 17:34:43 +0100

    Sessions db

---
+++ migrations/Version20220407172029.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220407172029 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('
+            CREATE TABLE sessions (
+                sess_id VARCHAR(255) NOT NULL PRIMARY KEY,
+                sess_data BLOB NOT NULL,
+                sess_lifetime BIGINT NOT NULL,
+                sess_time BIGINT NOT NULL
+            )
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE sessions');
+    }
+}
